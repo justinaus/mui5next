e95c5b543365cd0af975567dcb5a9f3262d80053
bonkook koo <bonkook.koo@sirloin.co.kr>
2021-09-24 15:17:35 +0900

    Update mui ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import type { AppProps } from 'next/app';
+import React from 'react';
+
+import createEmotionCache from '../styles/createEmotionCache';
+import { theme } from '../styles/theme';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
-export default MyApp
+
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: MyAppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+export default MyApp;
