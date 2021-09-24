ffa51bd4f61ce5fdd3b90f6f77af090f2b215c6a
bonkook koo <bonkook.koo@sirloin.co.kr>
2021-09-24 14:50:39 +0900

    Add eslint plugin simple-import-sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,11 @@ module.exports = {
     node: true,
   },
   extends: ['next', 'plugin:prettier/recommended', 'eslint:recommended'],
-  plugins: ['unused-imports'],
+  plugins: ['unused-imports', 'simple-import-sort'],
   rules: {
     'unused-imports/no-unused-imports': 'warn',
     'no-unused-vars': 'warn',
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
   },
 };
